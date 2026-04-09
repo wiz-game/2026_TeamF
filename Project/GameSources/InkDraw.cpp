6e08249e6d7c17ba88780c8ab2648d6242926009
Asuka Suzuki <zeal24450196@fsgcl.jp>
2026-04-09 13:21:57 +0900

    色が薄くなるようにする処理を追加

--- Project/GameSources/InkDraw.cpp
+++ Project/GameSources/InkDraw.cpp
@@ -17,14 +17,28 @@ namespace basecross{
 		m_transform->SetScale(Vec3(m_scaleX, m_scaleY, 0.5f));
 		m_draw = AddComponent<PNTStaticDraw>();
 		m_draw->SetMeshResource(L"DEFAULT_SQUARE");
-		m_draw->SetDiffuse(Col4(0, 0, 0, 1));
-		m_draw->SetEmissive(Col4(0, 0, 0, 1));
+		m_draw->SetDiffuse(Col4(m_color));
+		m_draw->SetEmissive(Col4(m_color));
 
 		//auto coll = AddComponent<CollisionObb>();
 	}
 
 	void InkDraw::OnUpdate()
 	{
+	}
+
+	void InkDraw::OnDraw()
+	{
+		float downcolor = 0.5f;
+
+		m_color.x -= downcolor;
+		m_color.y -= downcolor;
+		m_color.z -= downcolor;
+
+		if(m_draw){
+			m_draw->SetDiffuse(Col4(m_color));
+			m_draw->SetEmissive(Col4(m_color));
+		}
 	}
 }
 //end basecross
