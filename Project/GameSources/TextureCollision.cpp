ff6b441a72c8d5ef6650ff5701fa8ada5097cf0d
munaharu <rora.m.241217@gmail.com>
2026-04-10 22:21:06 +0900

    リリース版を実行できるように(exeと同じディレクトリにSDL3.dllを置いといてください)

--- Project/GameSources/TextureCollision.cpp
+++ Project/GameSources/TextureCollision.cpp
@@ -6,6 +6,8 @@
 #include "stdafx.h"
 #include "Project.h"
 #include <filesystem>
+#include <queue>
+
 namespace basecross{
 	TextureCollision::TextureCollision(const shared_ptr<GameObject>& ptr):Collision(ptr){}
 
@@ -44,18 +46,19 @@ namespace basecross{
 		return data;
 	}
 
-	vector<vector<UINT>>& TextureCollision::CreateAlphaMask() {
+	vector<TextureCollision::MaskData> TextureCollision::CreateAlphaMask(CoordContext& coordContext) {
 		auto& app = App::GetApp();
 		auto deviceResource = app->GetDeviceResources();
 		auto device = deviceResource->GetD3DDevice();
 		auto context = deviceResource->GetD3DDeviceContext();
 
+		vector<MaskData> errorList = {};
 		//設定されているテスクチャデータを取得(本来はインクのSRVをここで取得)
 		ID3D11Texture2D* srvTexture = nullptr;
 		D3D11_TEXTURE2D_DESC srvDesc;
 		GetSrvResource(&srvTexture, &srvDesc);
 		if (!srvTexture) {
-			return;
+			return errorList;
 		}
 
 		//読み取り用データを作成
@@ -67,7 +70,7 @@ namespace basecross{
 		ID3D11Texture2D* stagingTexture = nullptr;
 		auto result = device->CreateTexture2D(&desc, nullptr, &stagingTexture);
 		if (FAILED(result)) {
-			return;
+			return errorList;
 		}
 		//データをコピー
 		context->CopyResource(stagingTexture, srvTexture);
@@ -76,24 +79,23 @@ namespace basecross{
 		UINT rowPitch;
 		uint8_t* data = ReadColorData(context, stagingTexture, rowPitch);
 		if (!data) {
-			return;
+			return errorList;
 		}
-		UINT width = desc.Width;
-		UINT height = desc.Height;
+		coordContext.m_SizeX = desc.Width;
+		coordContext.m_SizeY = desc.Height;
 
-		vector<vector<UINT>> alphaMasks;
+		vector<MaskData> alphaMasks;
 		//テスト出力
 		std::filesystem::path path = "test.txt";
 
 		ofstream ofs(path);
 
 		//データのα値をもとに2値化
-		for (int y = 0; y < height; y++) {
-			alphaMasks.push_back({});
-			for (int x = 0; x < width; x++) {
+		for (int y = 0; y < coordContext.m_SizeY; y++) {
+			for (int x = 0; x < coordContext.m_SizeX; x++) {
 				uint8_t a = data[y * rowPitch + x * 4 + 3];
 				UINT mask = (a > 0 ? 1 : 0);
-				alphaMasks[y].push_back(mask);
+				alphaMasks.push_back({ mask, false });
 				ofs << mask;
 			}
 			ofs << endl;
@@ -104,12 +106,74 @@ namespace basecross{
 		return alphaMasks;
 	}
 
+	vector<int> TextureCollision::BfsTree(vector<MaskData>& masks, const CoordContext& context, int start) {
+		vector<int> area;
+		queue<int> searchQueue;
+		searchQueue.push(start);
+
+		while (!searchQueue.empty()) {
+			int search = searchQueue.front();
+			searchQueue.pop();
+			area.push_back(search);
+
+			vector<int> searchIndex = {
+				search + context.m_SizeX,
+				search - context.m_SizeX
+			};
+			int coordX, coordY;
+			IndexToCoord(search, context.m_SizeX, coordX, coordY);
+			if (coordX > 0) {
+				searchIndex.push_back(search - 1);
+			}
+			if (coordX < context.m_SizeX - 1) {
+				searchIndex.push_back(search + 1);
+			}
+
+			for (auto& index : searchIndex) {
+				if (index < 0 || index >= masks.size()) {
+					continue;
+				}
+				int mask = masks[index].m_Mask;
+				bool isVisited = masks[index].m_IsVisited;
+
+				if (mask != 0 && !isVisited) {
+					masks[index].m_IsVisited = true;
+					searchQueue.push(index);
+				}
+			}
+
+		}
+		return area;
+	}
+
 	void TextureCollision::CreateMeshCollision() {
-		vector<vector<UINT>> alphaMasks = CreateAlphaMask();
+		CoordContext context = CoordContext();
+		vector<MaskData> alphaMasks = CreateAlphaMask(context);
+
+		vector<vector<int>> maskAreaGroup;
+		
+		//領域ごとに分割(この領域の数のメッシュを生成する)
+		for (int i = 0; i < alphaMasks.size(); i++) {
+			int x = 0, y = 0;
+			IndexToCoord(i, context.m_SizeX, x, y);
+			int mask = alphaMasks[i].m_Mask;
+			bool isVisited = alphaMasks[i].m_IsVisited;
+
+			if (mask != 0 && !isVisited) {
+				auto area = BfsTree(alphaMasks, context, i);
+				maskAreaGroup.push_back(area);
+			}
+		}
 
 
 	}
-
+	void TextureCollision::IndexToCoord(int index, int width, int& x, int& y) {
+		x = index % width;
+		y = index / width;
+	}
+	void TextureCollision::CoordToIndex(int& index, int x, int y, int width) {
+		index = y * width + x;
+	}
 
 	bool TextureCollision::SimpleCollisionCall(const shared_ptr<Collision>& Src) {
 		return false;
