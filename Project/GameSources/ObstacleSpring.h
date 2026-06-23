7cbb02c1c26a2ed0a0c86a62264b1cf3d6d736da
Asuka Suzuki <zeal24450196@fsgcl.jp>
2026-06-23 14:47:18 +0900

    新規妨害オブジェクトを追加してテクスチャを適用

--- Project/GameSources/ObstacleSpring.h
+++ Project/GameSources/ObstacleSpring.h
@@ -15,6 +15,10 @@ namespace basecross{
 	class ObstacleSpring : public GameObject
 	{
 	public:
+
+		std::shared_ptr<PNTStaticDraw> m_draw; // ドローコンポーネント
+		std::shared_ptr<Transform> m_transform; // トランスフォームコンポーネント
+
 		// 構築と破棄
 		ObstacleSpring(const shared_ptr<Stage>& stage) :
 			GameObject(stage)
@@ -25,7 +29,7 @@ namespace basecross{
 		}
 
 		virtual void OnCreate() override; // 初期化
-		//virtual void OnUpdate() override; // 更新
+		virtual void OnUpdate() override; // 更新
 		//virtual void OnDraw() override; // 描画
 	};
 
