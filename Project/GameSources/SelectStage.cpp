1afa672e81e6507023e7ffe239bd20495fe8be8d
Kumagai Ayumu <zeal23300099@fsgcl.jp>
2026-06-30 15:49:59 +0900

    更新

--- Project/GameSources/SelectStage.cpp
+++ Project/GameSources/SelectStage.cpp
@@ -137,7 +137,7 @@ namespace basecross {
 			m_sSelectBGM = nullptr;
 
 			//PostEvent(0.2f, GetThis<SelectStage>(), app->GetScene<Scene>(), L"ToProtoStage");
-			PostEvent(0.0f, GetThis<ObjectInterface>(), app->GetScene<Scene>(), L"ToGameStage", make_shared<int>(m_SelectIndex));
+			PostEvent(0.3f, GetThis<ObjectInterface>(), app->GetScene<Scene>(), L"ToGameStage", make_shared<int>(m_SelectIndex));
 		}
 	}
 
